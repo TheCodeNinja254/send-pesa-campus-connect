c97b8f6db4b404d8977348b20bde20a2a95774bf
fredrick <fmmbugua@safaricom.co.ke>
2023-11-25 09:48:40 +0300

    Add fetch and axios with API calls

--- src/components/action_area/my_transactions/index.tsx
+++ src/components/action_area/my_transactions/index.tsx
@@ -1,8 +1,37 @@
 import { Box, useTheme } from "@mui/material";
 import { TransactionCard } from "@/components/common";
 
+const apiUrl = "https://private-57b77-quikkapi.apiary-mock.com/transactions";
+const accessToken =
+  "eyjsadlkdskl98iujnmsdjsd090iujnmsdsjsdmsd098u345tjfdksjd908utrjfekdsm340r9fuidsjkmijhbnj908uhjnkmnjoijhbhu90oiujhkjnbnju9oijknmkloi8765trfghjknmbhy6rtyujhbnjui76ytgbhujhbgtyuhjkiu87689ioujhjkuyghjuhygvhby78987yhuikjhhujkhuytgfvfcgyui987s=";
+
 const MyTransactions = () => {
   const theme = useTheme();
+
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json", // Adjust the content type as needed
+  };
+
+  fetch(apiUrl, {
+    method: "GET",
+    headers,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Data:", data);
+      // Handle the data as needed
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle errors
+    });
 
   return (
     <Box sx={{ margin: theme.spacing(2) }}>
--- src/components/forms/send_money_form/index.tsx
+++ src/components/forms/send_money_form/index.tsx
@@ -1,6 +1,11 @@
 import { Form as FormikForm, Formik } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
 import { Button, Grid, TextField } from "@mui/material";
+
+const apiUrl = "https://private-57b77-quikkapi.apiary-mock.com/transactions";
+const accessToken =
+  "eyjsadlkdskl98iujnmsdjsd090iujnmsdsjsdmsd098u345tjfdksjd908utrjfekdsm340r9fuidsjkmijhbnj908uhjnkmnjoijhbhu90oiujhkjnbnju9oijknmkloi8765trfghjknmbhy6rtyujhbnjui76ytgbhujhbgtyuhjkiu87689ioujhjkuyghjuhygvhby78987yhuikjhhujkhuytgfvfcgyui987s=";
 
 const SendMoneySchema = Yup.object().shape({
   recipientId: Yup.string()
@@ -15,8 +20,9 @@ const SendMoneySchema = Yup.object().shape({
 });
 
 const SendMoneyForm = () => {
-  const handleSendMoney = () => {
-    console.log("We are going to send money here. Yaay!");
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json", // Adjust the content type as needed
   };
 
   return (
@@ -27,7 +33,31 @@ const SendMoneyForm = () => {
         amount: 0,
       }}
       validationSchema={SendMoneySchema}
-      onSubmit={handleSendMoney}
+      onSubmit={(values) => {
+        // Make the POST request using Axios
+        axios
+          .post(
+            apiUrl,
+            {
+              recipientId: values.recipientId,
+              recipientIdType: "mobilePhoneNumber",
+              amount: values.amount,
+            },
+            {
+              headers,
+            }
+          )
+          .then((response) => {
+            console.log("Response:", response.data);
+            // Handle the response data as needed
+          })
+          .catch((error) => {
+            console.error(
+              "Error:",
+              error.response ? error.response.data : error.message
+            );
+          });
+      }}
     >
       {({ errors, setFieldValue, values, isValid }) => (
         <FormikForm>
