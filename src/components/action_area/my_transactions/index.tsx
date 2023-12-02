dfd7afa14a64c6fbdd95978e5367f94cf9facef1
fredrick <fmmbugua@safaricom.co.ke>
2023-12-02 11:45:31 +0300

    Add Axios and Fetch for API Add spinner

--- src/components/action_area/my_transactions/index.tsx
+++ src/components/action_area/my_transactions/index.tsx
@@ -1,12 +1,75 @@
 import { Box, useTheme } from "@mui/material";
-import { TransactionCard } from "@/components/common";
+import { SuspenseLoader, TransactionCard } from "@/components/common";
+import React, { useState } from "react";
+import { Labrada } from "next/dist/compiled/@next/font/dist/google";
+
+const apiURL = process.env.NEXT_PUBLIC_API_ENDPOINT_URL as string; // as string to specify the type to expect here.
+const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN as string; // as string to specify the type to expect here.
+
+// TypeScript types definition here.
+type ApiResponseInterface = {
+  transactionId: number;
+  recipientEmail: string;
+  recipientPhoneNumber: string;
+  amount: number;
+  currency: string;
+  timestamp: string;
+};
 
 const MyTransactions = () => {
   const theme = useTheme();
 
+  const [apiResponse, setApiResponse] = useState<ApiResponseInterface[]>([]);
+
+  // spinner status
+  const [loading, setLoading] = useState(false);
+
+  React.useEffect(() => {
+    // start spinner
+    setLoading(true);
+
+    // set our headers to cater for Authorization
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    };
+
+    fetch(apiURL, {
+      method: "GET",
+      headers,
+    })
+      .then((res) => {
+        // stop spinner
+        setLoading(false);
+
+        if (!res.ok) {
+          throw new Error("There was an error fetching transactions");
+        }
+
+        return res.json();
+      })
+      .then((data) => {
+        // keep data within the state
+        setApiResponse(data.body.transactions);
+      })
+      .catch((error) => {
+        // stop spinner
+        setLoading(false);
+
+        // do something with the error
+        // linting will throw an error since it is bad practise to leave console.log() in your code
+        console.log(error);
+      });
+  }, []);
+
+  if (loading) return <SuspenseLoader />;
+
   return (
     <Box sx={{ margin: theme.spacing(2) }}>
-      <TransactionCard />
+      {apiResponse &&
+        apiResponse.map((tr: ApiResponseInterface) => (
+          <TransactionCard key={tr.transactionId} transaction={tr} />
+        ))}
     </Box>
   );
 };
--- src/components/forms/send_money_form/index.tsx
+++ src/components/forms/send_money_form/index.tsx
@@ -1,12 +1,26 @@
 import { Form as FormikForm, Formik } from "formik";
 import * as Yup from "yup";
-import { Button, Grid, TextField } from "@mui/material";
+import axios from "axios";
+import {
+  Avatar,
+  Box,
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { useState } from "react";
+import { CancelRounded, Check } from "@mui/icons-material";
+
+const apiURL = process.env.NEXT_PUBLIC_API_ENDPOINT_URL as string;
+const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN as string;
 
 const SendMoneySchema = Yup.object().shape({
   recipientId: Yup.string()
     .min(10, "Mobile number should have 10 digits")
     .max(10, "Mobile number should have 10 digits")
-    .matches(/^[0-9]*$/, "Invalid")
+    .matches(/^[0-9]*$/, "Invalid mobile number.")
     .required("Who are we sending money to? Please enter."),
   amount: Yup.number()
     .min(10, "Minimum allowed amount is Ksh. 10")
@@ -15,70 +29,149 @@ const SendMoneySchema = Yup.object().shape({
 });
 
 const SendMoneyForm = () => {
-  const handleSendMoney = () => {
-    console.log("We are going to send money here. Yaay!");
+  const theme = useTheme();
+
+  const [hasResponse, setHasResponse] = useState({
+    status: false,
+    open: false,
+    message: "",
+  });
+
+  const { status, open, message } = hasResponse;
+
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+  };
+
+  const handleSendMoney = (passedValues: any) => {
+    axios
+      .post(
+        apiURL,
+        {
+          recipientId: passedValues.recipientId,
+          recipientIdType: passedValues.recipientIdType,
+          amount: passedValues.amount,
+        },
+        {
+          headers,
+        }
+      )
+      .then((res) => {
+        if (res.data.header.responseCode === 200) {
+          setHasResponse({
+            status: true,
+            open: true,
+            message: "Transaction successful",
+          });
+        } else {
+          setHasResponse({
+            status: false,
+            open: true,
+            message: "Transaction failed",
+          });
+        }
+      })
+      .catch(() => {
+        setHasResponse({
+          status: false,
+          open: true,
+          message: "Something went wrong! Please try again later.",
+        });
+      });
   };
 
   return (
-    <Formik
-      initialValues={{
-        recipientId: "",
-        recipientIdType: "mobilePhoneNumber",
-        amount: 0,
-      }}
-      validationSchema={SendMoneySchema}
-      onSubmit={handleSendMoney}
-    >
-      {({ errors, setFieldValue, values, isValid }) => (
-        <FormikForm>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                error={!!errors.recipientId}
-                fullWidth
-                helperText={errors.recipientId || null}
-                label="Receipeint Mobile Number"
-                name="recipientId"
-                onChange={(e) => {
-                  setFieldValue("recipientId", e.target.value, true);
+    <>
+      {open ? (
+        <>
+          <Box display="flex" alignContent="center" justifyContent="center">
+            {status ? (
+              <Avatar
+                sx={{
+                  bgcolor: theme.palette.primary.main,
+                  marginTop: theme.spacing(3),
+                  marginBottom: theme.spacing(2),
                 }}
-                type="text"
-                value={values.recipientId}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                error={!!errors.amount}
-                fullWidth
-                helperText={errors.amount || null}
-                label="Amount to send (Ksh.)"
-                name="amount"
-                onChange={(e) => {
-                  setFieldValue("amount", e.target.value, true);
-                }}
-                type="number"
-                value={values.amount}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                color="primary"
-                disableElevation
-                disabled={!isValid}
-                fullWidth
-                size="large"
-                type="submit"
-                variant="contained"
               >
-                Send Pesa Now
-              </Button>
-            </Grid>
-          </Grid>
-        </FormikForm>
+                <Check fontSize="large" />
+              </Avatar>
+            ) : (
+              <Avatar
+                sx={{
+                  bgcolor: theme.palette.error.main,
+                  marginTop: theme.spacing(3),
+                  marginBottom: theme.spacing(2),
+                }}
+              >
+                <CancelRounded fontSize="large" />
+              </Avatar>
+            )}
+          </Box>
+          <Typography align="center">{message}</Typography>
+        </>
+      ) : (
+        <Formik
+          initialValues={{
+            recipientId: "",
+            recipientIdType: "mobilePhoneNumber",
+            amount: 0,
+          }}
+          validationSchema={SendMoneySchema}
+          onSubmit={(values) => handleSendMoney(values)}
+        >
+          {({ errors, setFieldValue, values, isValid }) => (
+            <FormikForm>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    error={!!errors.recipientId}
+                    fullWidth
+                    helperText={errors.recipientId || null}
+                    label="Receipeint Mobile Number"
+                    name="recipientId"
+                    onChange={(e) => {
+                      setFieldValue("recipientId", e.target.value, true);
+                    }}
+                    type="text"
+                    value={values.recipientId}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    error={!!errors.amount}
+                    fullWidth
+                    helperText={errors.amount || null}
+                    label="Amount to send (Ksh.)"
+                    name="amount"
+                    onChange={(e) => {
+                      setFieldValue("amount", e.target.value, true);
+                    }}
+                    type="number"
+                    value={values.amount}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    color="primary"
+                    disableElevation
+                    disabled={!isValid}
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                  >
+                    Send Pesa Now
+                  </Button>
+                </Grid>
+              </Grid>
+            </FormikForm>
+          )}
+        </Formik>
       )}
-    </Formik>
+    </>
   );
 };
 
