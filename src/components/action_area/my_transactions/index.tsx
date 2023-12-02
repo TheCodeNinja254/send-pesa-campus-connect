0e637ee8987ca28fb75cb841b038ca9ca18adcc2
fredrick <fmmbugua@safaricom.co.ke>
2023-12-02 12:25:33 +0300

    Add Axios and Fetch for API Add spinner

--- src/components/action_area/my_transactions/index.tsx
+++ src/components/action_area/my_transactions/index.tsx
@@ -1,7 +1,6 @@
 import { Box, useTheme } from "@mui/material";
 import { SuspenseLoader, TransactionCard } from "@/components/common";
 import React, { useState } from "react";
-import { Labrada } from "next/dist/compiled/@next/font/dist/google";
 
 const apiURL = process.env.NEXT_PUBLIC_API_ENDPOINT_URL as string; // as string to specify the type to expect here.
 const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN as string; // as string to specify the type to expect here.
@@ -52,13 +51,12 @@ const MyTransactions = () => {
         // keep data within the state
         setApiResponse(data.body.transactions);
       })
-      .catch((error) => {
+      .catch(() => {
         // stop spinner
         setLoading(false);
 
         // do something with the error
         // linting will throw an error since it is bad practise to leave console.log() in your code
-        console.log(error);
       });
   }, []);
 
